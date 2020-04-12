db16034904ea67c53c72a5903e76e3af3b2e97c2
GotamDahiya <apollo2562000@gmail.com>
2020-04-12 20:27:42 +0530

    Basic server program

---
+++ server.go
@@ -0,0 +1,16 @@
+// Run http:/localhost:8080 in the web browser
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
